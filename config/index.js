85c9b777b36ce10e5cfc9475939d0a6e0a199876
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-12 20:59:17 -0700

    added ternary for heroku deployment

--- config/index.js
+++ config/index.js
@@ -1,18 +1,7 @@
 
-if (MONGODB_URI){
-  db = require('mongoose').connect(MONGODB_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false
-    });
-} else {
-db = require('mongoose').connect('mongodb://localhost/workout',
+module.exports = require('mongoose').connect( MONGODB_URI ? MONGODB_URI : 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
   });
-}
-
-module.exports = db;
