1228883ad55def1bc609e2e48d1b76bd15f537cc
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-12 21:04:10 -0700

    changed to heroku db

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 
-module.exports = require('mongoose').connect( MONGODB_URI ? MONGODB_URI : 'mongodb://localhost/workout',
+module.exports = require('mongoose').connect('mongodb://heroku_dhpmgw0d:m00qt80dpoh51cbdk5ks4b5gl6@ds241658.mlab.com:41658/heroku_dhpmgw0d',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
