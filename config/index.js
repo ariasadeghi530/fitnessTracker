6253be4f5fe33301639af6e38a99a39867b5e75f
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-12 20:54:18 -0700

    added heroku db

--- config/index.js
+++ config/index.js
@@ -1,7 +1,18 @@
 
-module.exports = require('mongoose').connect('mongodb://localhost/workout',
+if (MONGODB_URI){
+  db = require('mongoose').connect(MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+} else {
+db = require('mongoose').connect('mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
   });
+}
+
+module.exports = db;
