6effdb149b570a6ccb32badd8b3ba6a888abc655
Aria Sadeghi <ariasadeghi@gmail.com>
2020-03-13 10:20:37 -0700

    added dotenv for db connection

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 
-module.exports = require('mongoose').connect('mongodb://heroku_dhpmgw0d:m00qt80dpoh51cbdk5ks4b5gl6@ds241658.mlab.com:41658/heroku_dhpmgw0d',
+module.exports = require('mongoose').connect( process.env.MONGODB_URI || process.env.LOCAL_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
